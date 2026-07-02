1ff5f975d7d0fbc81eb3af66f8455a78b18a1abb
Arash <62180768+Arash-11@users.noreply.github.com>
2026-07-02 15:11:56 -0400

    add getsockname.c

---
+++ networking/getsockname.c
@@ -0,0 +1,46 @@
+/*
+Using getsockname() to show that, if listen() is called on a TCP socket
+without first calling bind(), the socket is assigned an ephemeral port.
+*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(void) {
+    for (int i = 0; i < 3; i++) {
+        int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+        // skip error checking for socket()
+
+        listen(sockfd, SOMAXCONN);
+        // skip error checking for listen()
+
+        struct sockaddr sa;
+        socklen_t addr_len = sizeof(sa);
+        getsockname(sockfd, &sa, &addr_len);
+        // skip error checking for getsockname()
+
+        char host[NI_MAXHOST];
+        char port[NI_MAXSERV];
+        getnameinfo(&sa, sa.sa_len, host, sizeof(host), port, sizeof(port), 0);
+        // skip error checking for getnameinfo()
+
+        printf("host = %s\n", host);
+        printf("port = %s\n", port);
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+Example output:
+    host = 0.0.0.0
+    port = 61038
+
+    host = 0.0.0.0
+    port = 61039
+
+    host = 0.0.0.0
+    port = 61040
+*/
